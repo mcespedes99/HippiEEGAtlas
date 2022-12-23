faca84184f9e8a1eac426b1b9ad56c570a5c6a90
Mauricio Cespedes Tenorio <mcespedes99@gmail.com>
2022-12-22 20:42:34 -0500

    Working version bipolar epochs

--- epochs/hippieeg/code/epochs.py
+++ epochs/hippieeg/code/epochs.py
@@ -7,41 +7,49 @@ from file_manager import create_EDF, find
 import argparse
 import re
 import shutil
+import os
 
 parser = argparse.ArgumentParser(description='Script to extract epochs\
                                              from a *.edf file.')
-parser.add_argument("I", metavar='INPUT',
+parser.add_argument("-p", "--processes" ,metavar='PROCESESS',
+                    nargs = 1,
+                    help='Number of processes per patient.')
+parser.add_argument("-i", "--input", metavar='INPUT',
+                    nargs = '*',
                     help='Path to subject directory')
-parser.add_argument("O", metavar='OUTPUT',
+parser.add_argument("-t", "--tsv", metavar='INPUT_TSV',
+                    nargs = '*',
+                    help='Path to TSV file')
+parser.add_argument("-o", "--output" ,metavar='OUTPUT',
+                    nargs = 1,
                     help='Path to the output file.')
 
 args = parser.parse_args()
 
-input_file = args.I
-output_file = args.O
-
+input_files = args.input
+output_dir = args.output[0]
+processes = int(args.processes[0])
+tsv_file = args.tsv[0]
+print(tsv_file)
+print(output_dir)
 # Import file:
 try:
-    # Copy tsv file
-    shutil.copyfile(input_file, output_file)
     # Create edf
-    # Create input path:
-    pattern_del = r'sub-\d{3}_ses-\d{3}_task-(.+)_run-\d{2}_channels.tsv'
-    del_str = re.search(pattern=pattern_del, string=input_file).group()
-    input_path_ieeg = input_file.replace(del_str,'')
-    # Create output path:
-    del_str = re.search(pattern=pattern_del, string=input_file).group()
+    # Create pattern to find input file name
+    pattern_complete = r'sub-\d{3}_ses-\d{3}_task-(.+)_run-\d{2}_ieeg.edf'
+    # Create patterns to write output files
+    pattern_dir = r'ses-\d{3}/.+/'
     pattern_ieeg = r'sub-\d{3}_ses-\d{3}_task-(.+)_run-\d{2}_ieeg'
-    output_path_ieeg = output_file.replace(del_str,'')
-    # Look for edf files:
-    list_files = find("*.edf", input_path_ieeg)
-    for edf in list_files:
+    # Loop through different edf files given by snakebids
+    for edf in input_files:
+        # I'm planning to change this to use the tsv files to check the annotations as
+        # loading the edf files takes a while (to make it more efficient)
         f = pyedflib.EdfReader(edf)
         # Get labels:
         labels = f.getSignalLabels()
-
         # Create df with annotations
         annot = f.readAnnotations()
+        f.close()
         annot = {
             'Onset': annot[0],
             'Duration': annot[1],
@@ -52,15 +60,27 @@ try:
         # Find time stamps where the 'awake trigger' event is happening
         id = annot['event'].str.contains('awake trigger', case=False)
         time_stamps = annot.Onset.to_numpy()[id]
-
+        # If any event is found, then the pipeline is run
         if time_stamps.size > 0:
+            # Copy file to local scratch
+            shutil.copy(edf, '/tmp/')
+            new_edf ='/tmp/'+re.search(pattern_complete, edf).group()
+            # Build output folder/file
+            output_path_ieeg = re.search(pattern_dir, edf).group()
             out_file_name = re.search(pattern=pattern_ieeg, string=edf).group()
-            if output_path_ieeg.endswith('/'):
+            if output_dir.endswith('/'):
+                output_path_ieeg = output_dir + output_path_ieeg
+                os.makedirs(output_path_ieeg)
                 out_path_name = output_path_ieeg+out_file_name+'_epoch.edf'
             else:
-                out_path_name = output_path_ieeg+'/'+out_file_name+'_epoch.edf'
-            # Here call function
-            create_EDF(f, time_stamps, out_path_name)
+                output_path_ieeg = output_dir+'/'+output_path_ieeg
+                os.makedirs(output_path_ieeg)
+                out_path_name = output_path_ieeg+out_file_name+'_epoch.edf'
+            # Extract info about electrodes positions
+            elec_pos = pd.read_csv(tsv_file, sep='\t')
+            # Here call function to create new EDF file
+            create_EDF(new_edf, time_stamps, elec_pos, out_path_name, processes)
+            os.remove(new_edf)
 except FileNotFoundError:
     raise FileNotFoundError('The *.edf file was not found in the indicated \
         path.')
--- epochs/hippieeg/code/file_manager.py
+++ epochs/hippieeg/code/file_manager.py
@@ -4,22 +4,91 @@ import os, fnmatch
 from collections import OrderedDict
 from pathlib import Path
 from typing import Union
+from functools import partial
+import traceback
+from multiprocessing.pool import Pool
+import re
 
-def create_EDF(edf_in, time_stamps, out_path):
-    # First import labels
-    labels = edf_in.getSignalLabels()
-    # Create file:
-    edf_out = pyedflib.EdfWriter(out_path, len(labels), file_type=pyedflib.FILETYPE_EDFPLUS)
+# Function to look for timestamps
+def extract_time_ids(epoch_id, time_vector, timestamps_array, srate):
+    temp = np.asfortranarray(np.subtract(time_vector,timestamps_array[epoch_id]))
+    t_init_id = np.abs(temp).argmin() ## CANNOT BE ZERO, change to a not magic 
+    t_end_id = int(np.floor(t_init_id+240*srate+1)) # 4 min = 240 s
+    return (t_init_id, t_end_id)
+
+# Function to create bipolar channels from given unipolars
+def create_bipolar_comb(id, dict_key, channels_dict):
+    bipolar_chn = dict_key+channels_dict[dict_key][id]+'-'+channels_dict[dict_key][id+1] 
+    chn1_label = dict_key+channels_dict[dict_key][id]
+    chn2_label = dict_key+channels_dict[dict_key][id+1]
+    return (bipolar_chn, chn1_label, chn2_label)
+
+# Function to create a bipolar channel list from 
+def create_bipolars(electrodes_df, processes):
+    channels = dict((label,[]) for label in electrodes_df.orig_group.unique())
+    pattern = r'([A-Z]+)(\d+)'
+    electrode_labels = electrodes_df.label.values
+    # Extract channels info
+    for electrode in electrodes_df.label.values:
+        match = re.match(pattern, electrode, re.IGNORECASE)
+        channels[match.group(1)].append(match.group(2))
+    # print(channels)
+    # Create new list
+    bipolar_list = []
+    for key in  channels.keys():
+        with Pool(processes=processes) as pool:
+                bipolar_list = bipolar_list + pool.map(partial(create_bipolar_comb, dict_key=key, channels_dict=channels), 
+                                        list(range(len(channels[key])-1)))
+    return bipolar_list
+
+# Function to extract info from each channel
+def extract_channel_data(chn_number, edf_file, srate_data, time_ids, bipolar_list):
+    edf_in = pyedflib.EdfReader(edf_file)
+    # Get labels from original edf file
+    channels_labels = edf_in.getSignalLabels()
+    # Get indexes of channels
+    chn1_id = channels_labels.index(bipolar_list[chn_number][1])
+    chn2_id = channels_labels.index(bipolar_list[chn_number][2])
+    signal_chn1 = edf_in.readSignal(chn1_id)
+    signal_chn2 = edf_in.readSignal(chn2_id)
+    print(chn_number)
+    chn_data = np.array([], dtype=float)
+    for t_id in time_ids:
+        signal_bipolar = signal_chn1[t_id[0]:t_id[1]] - signal_chn2[t_id[0]:t_id[1]]
+        chn_data = np.hstack([chn_data, 
+                            signal_bipolar, 
+                            np.zeros(int(60*srate_data))])
+    # Deallocate space in memory
+    del signal_chn1
+    del signal_chn2
+    return chn_data
+
+# Function to extract headers for bipolar channels
+def extract_channel_header(chn_number, original_headers, bipolar_list, channels_labels):
+    # Get indexes of channels
+    chn1_id = channels_labels.index(bipolar_list[chn_number][1])
+    # Update header
+    chn_header = original_headers[chn1_id]
+    chn_header['label'] = bipolar_list[chn_number][0]
+    return chn_header
+
+# Function to create EDF file with bipolar data
+def create_EDF(edf_file, time_stamps, electrodes_position, out_path, processes):
     try:
+        edf_in = pyedflib.EdfReader(edf_file)
+        # First import labels
+        labels = edf_in.getSignalLabels()
+        # Create bipolar combinations
+        bipolar_channels = create_bipolars(electrodes_position, processes)
+        # Create file:
+        edf_out = pyedflib.EdfWriter(out_path, len(bipolar_channels), file_type=pyedflib.FILETYPE_EDFPLUS)
         # First set the data from the header of the edf file:
         edf_out.setHeader(edf_in.getHeader())
+        headers_orig = edf_in.getSignalHeaders()
         # f.datarecord_duration gives the value is sec and setDatarecordDuration receives it in units
         # of 10 ms. Therefore: setDatarecordDuration = datarecord_duration*10^6 / 10
-        # int(edf_in.datarecord_duration*(100000)))
         edf_out.setDatarecordDuration(int(edf_in.datarecord_duration*100000)) # This actually is used to set the sample frequency
         # Set each channel info:
-        # First create an empty list for the data
-        channel_data = [np.array([], dtype=float) for chn in range(len(labels))]
         # Sampling rate:
         srate = edf_in.getSampleFrequencies()[0]/edf_in.datarecord_duration
         # Build epochs
@@ -29,27 +98,37 @@ def create_EDF(edf_in, time_stamps, out_path):
         # Relative initial time for epochs
         t_0 = t[np.abs(np.subtract(t,time_stamps[0])).argmin()]
         edf_out.writeAnnotation(0, -1, "Recording starts")
-        # Change this for to 2 separate fors. The first should find and store the t_ids and the second one 
-        # should iterate across channels
-        t_ids = []
-        for epoch_id in range(time_stamps.size):
-            temp = np.asfortranarray(np.subtract(t,time_stamps[epoch_id]))
-            t_init_id = np.abs(temp).argmin() ## CANNOT BE ZERO, change to a not magic #
-            t_end_id = int(np.floor(t_init_id+240*srate+1)) # 4 min = 240 s
-            t_ids.append((t_init_id, t_end_id))
+        # Close file
+        edf_in.close()
+        # Create time ids
+        list_epochs_ids = list(range(time_stamps.size))
+        print('Time part')
+        # Extracting time indexes based on time stamps
+        with Pool(processes=processes) as pool:
+            t_ids = pool.map(partial(extract_time_ids, time_vector=t, timestamps_array=time_stamps, srate=srate), 
+                            list_epochs_ids)
+        # Write annotations at the different epochs times (beginning and end of epochs)
+        for id, (t_init_id, t_end_id) in enumerate(t_ids):
             # Write annotations
-            edf_out.writeAnnotation(t[t_init_id]-t_0, -1, f"Epoch #{epoch_id+1} starts.")
-            edf_out.writeAnnotation(t[t_end_id]-t_0, -1, f"Epoch #{epoch_id+1} ends.")
+            edf_out.writeAnnotation(t[t_init_id]-t_0, -1, f"Epoch #{id+1} starts.")
+            edf_out.writeAnnotation(t[t_end_id]-t_0, -1, f"Epoch #{id+1} ends.")
         # Deallocate space in memory
         del t
-        for chn in range(len(labels)):
-            signal = edf_in.readSignal(chn)
-            for t_id in t_ids:
-                channel_data[chn] = np.hstack([channel_data[chn], signal[t_id[0]:t_id[1]], np.zeros(int(60*srate))])
-                # Deallocate space in memory
-            del signal
-        # print(channel_data)
-        headers = edf_in.getSignalHeaders()
+        # Extract channel information:
+        chn_lists = range(len(bipolar_channels))
+        print('Channel part')
+        # Create bipolar signals:
+        with Pool(processes=processes) as pool2:
+            channel_data = pool2.map(partial(extract_channel_data, edf_file=edf_file, 
+                                            srate_data=srate, time_ids=t_ids,
+                                            bipolar_list=bipolar_channels), chn_lists)
+        # Create headers:
+        with Pool(processes=processes) as pool2:
+            headers = pool2.map(partial(extract_channel_header, 
+                                      original_headers=headers_orig,
+                                      bipolar_list=bipolar_channels,
+                                      channels_labels=labels), chn_lists)
+        # Edit headers to make them compliant with edf files
         for header in headers:
             header['physical_max'] = int(header['physical_max'])
             header['physical_min'] = int(header['physical_min'])
@@ -57,12 +136,14 @@ def create_EDF(edf_in, time_stamps, out_path):
                 header['physical_max'] = int(str(header['physical_max'])[0:8])
             if len(str(header['physical_min']))>8:
                 header['physical_min'] = int(str(header['physical_min'])[0:8])
+        
         edf_out.setSignalHeaders(headers)
         edf_out.writeSamples(channel_data)
         edf_out.close()
     except Exception:
         traceback.print_exc()
         edf_out.close()
+        edf_in.close()
 
 def find(pattern, path):
     result = []
@@ -120,7 +201,9 @@ def bids_folder(
             None,
             [
                 str(root) if root else None,
-                f"sub-{subject}" if subject and include_subject_dir else None
+                f"sub-{subject}" if subject and include_subject_dir else None,
+                #f"ses-{session}" if session and include_session_dir else None,
+                #datatype,
             ],
         )
     )
